515114ab9ba82a070933d8ede25a552dbd9f2def
Iván Del Valle <ivandelvalle@Ivans-MacBook-Pro.local>
2024-07-28 15:56:27 -0600

    Handle failed responses

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 # SETTINGS
+import requests
+
 from robocorp import workitems
 from robocorp.tasks import task
+
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables
@@ -131,4 +134,20 @@ def process_traffic_data():
     """Process the work items."""
     for item in workitems.inputs:
         traffic_data = item.payload["traffic_data"]
-        print(traffic_data)
+        if traffic_data["country"] == 3:
+            status, json_response = post_traffic_data_to_sales_system(traffic_data)
+
+            if status == 200:
+                item.done()
+            else:
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=json_response["message"],
+                )
+
+
+def post_traffic_data_to_sales_system(traffic_data):
+    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
+    response = requests.post(url, json=traffic_data)
+    return response.status_code, response.json()
