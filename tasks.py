275cadbe9ef7a8083dc69b82584ee263f52e9208
Iván Del Valle <ivandelvalle@Ivans-MacBook-Pro.local>
2024-07-28 14:28:32 -0600

    Get latest data by country.

--- tasks.py
+++ tasks.py
@@ -1,13 +1,20 @@
+# SETTINGS
+
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables
+import json
+
+# KEYWORDS
 
 http = HTTP()
-json = JSON()
+json_rpa = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# TASKS
 
 
 @task
@@ -16,6 +23,8 @@ def produce_traffic_data():
     print("Producing.")
     download_traffic_data()
     data_table = load_traffic_data_as_table()
+    filtered_data = filter_and_sort_traffic_data(data_table)
+    get_latest_data_by_country(filtered_data)
 
 
 @task
@@ -36,18 +45,18 @@ def download_traffic_data():
 
 def load_traffic_data_as_table():
     """Get data from output folder and transform it to a table."""
-    data = json.load_json_from_file("output/traffic.json")
+    data = json_rpa.load_json_from_file("output/traffic.json")
     data_table = table.create_table(data["value"])
     return data_table
 
 
 def filter_and_sort_traffic_data(data_table):
-    """Get data from file and filter it."""
+    """Get data from file to filter and sort it."""
     # Iterate data and filter the three columns.
     filtered_data = []
 
     # KEYS
-    rate_key = "NumericValues"
+    rate_key = "NumericValue"
     gender_key = "Dim1"
     year_key = "TimeDim"
     code_country_key = "SpatialDim"
@@ -68,6 +77,27 @@ def filter_and_sort_traffic_data(data_table):
 
     # SORT DATA BY TimeDim
     sorted_data = sorted(filtered_data, key=lambda x: x[year_key], reverse=True)
+
     return sorted_data
 
-    return filtered_data
+
+def get_latest_data_by_country(data):
+    """Get the latest data of each country in data array."""
+    # Group data by country code.
+    country_code_key = "SpatialDim"
+    latest_data = []
+    countries = set()
+
+    for row in data:
+        code = row[country_code_key]
+        country_code = {code}
+        exists = countries.intersection(country_code)
+
+        if exists:
+            pass
+        else:
+            countries.add(code)
+            latest_data.append(row)
+            pass
+
+    return latest_data
