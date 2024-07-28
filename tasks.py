a38ebecf3d7c010ed9a8262a6c141d19905cdbfc
Iván Del Valle <ivandelvalle@Ivans-MacBook-Pro.local>
2024-07-28 13:10:31 -0600

    Filter and sort the data.

--- tasks.py
+++ tasks.py
@@ -7,6 +7,8 @@ http = HTTP()
 json = JSON()
 table = Tables()
 
+TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
 
 @task
 def produce_traffic_data():
@@ -28,9 +30,8 @@ def consume_traffic_data():
 def download_traffic_data():
     """Download data from API and save it as a file in output folder."""
     url = "https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json"
-    target_file = "output/traffic.json"
 
-    http.download(url=url, target_file=target_file, overwrite=True)
+    http.download(url=url, target_file=TRAFFIC_JSON_FILE_PATH, overwrite=True)
 
 
 def load_traffic_data_as_table():
@@ -38,3 +39,35 @@ def load_traffic_data_as_table():
     data = json.load_json_from_file("output/traffic.json")
     data_table = table.create_table(data["value"])
     return data_table
+
+
+def filter_and_sort_traffic_data(data_table):
+    """Get data from file and filter it."""
+    # Iterate data and filter the three columns.
+    filtered_data = []
+
+    # KEYS
+    rate_key = "NumericValues"
+    gender_key = "Dim1"
+    year_key = "TimeDim"
+    code_country_key = "SpatialDim"
+
+    # VALUES
+    max_rate = 5.0
+    gender_value = "BTSX"
+
+    # FILTER DATA
+    for data in data_table:
+        if data[rate_key] < max_rate and data[gender_key] == gender_value:
+            item = {}
+            item[rate_key] = data[rate_key]
+            item[gender_key] = data[gender_key]
+            item[year_key] = data[year_key]
+            item[code_country_key] = data[code_country_key]
+            filtered_data.append(item)
+
+    # SORT DATA BY TimeDim
+    sorted_data = sorted(filtered_data, key=lambda x: x[year_key], reverse=True)
+    return sorted_data
+
+    return filtered_data
